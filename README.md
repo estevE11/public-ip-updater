1cbe850fd7e0e7321be4062d726c96d8ce35ac2b
Roger Esteve <Coconaut7@gmail.com>
2019-07-09 18:59:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# public-ip-updater
+Currently a discord bot that notifies when your public IP changes in case you're hosting any kind of server.
---
+++ bot.py
@@ -0,0 +1,38 @@
+import discord
+import requests
+import time
+
+current_time_millis = lambda: int(round(time.time() * 1000))
+
+TOKEN = 'NTk3ODk3MzA2MTI1MjM4MzMz.XSSTbw.rklvd7EoL5zRwNW_zcCgqnLb7-A'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    await check_ip()
+
+async def check_ip():
+    curr_ip = await get_ip()
+    print(curr_ip)
+    await update_ip(curr_ip)
+    old = current_time_millis()
+    while True:
+        if current_time_millis() - old > 60000:
+            new_ip = await get_ip()
+            if new_ip != curr_ip:
+                await update_ip(new_ip)
+            old = current_time_millis()
+
+async def get_ip():
+    return requests.get('http://ip.42.pl/raw').text
+
+async def update_ip(ip):
+    channel = client.get_channel(596775518536335387)
+    await channel.send(str(ip))
+
+client.run(TOKEN)
